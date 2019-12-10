072cac7d4f3b24e348e7c678cdfdf8b75e264c4f
ImoutoChan <kirirrin@gmail.com>
2019-12-10 10:01:47 +0300

    Add day 10

---
+++ AdventOdCode2019/Day10.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOdCode2019
+{
+    internal class Day10 : IAdventOfCodeDay
+    {
+        public string CalculatePart1(string inputFile)
+        {
+            var asteroids = GetAsteroids(inputFile);
+
+            var result = asteroids.Select(x => FindAllViewable(x, asteroids)).Max();
+
+            return result.ToString();
+        }
+
+        public string CalculatePart2(string inputFile)
+        {
+            var asteroids = GetAsteroids(inputFile);
+
+            var bestLocation = asteroids.Select(x => (x, FindAllViewableOrdered(x, asteroids))).OrderByDescending(x => x.Item2.Count()).First().x;
+            
+            var counter = 200;
+
+            while (true)
+            {
+                var result = FindAllViewableOrdered(bestLocation, asteroids);
+                if (counter - result.Count > 0)
+                {
+                    counter -= result.Count;
+                    asteroids = asteroids.Except(result.Select(x => x.Value)).ToList();
+                }
+                else
+                {
+                    var res = result[counter - 1].Value;
+                    return $"{res.X} {res.Y} {res.X * 100 + res.Y}";
+                }
+            }
+
+            return "Error!";
+        }
+
+        private int FindAllViewable(Asteroid forAsteroid, List<Asteroid> asteroids)
+        {
+            var result = 0;
+            var check = new Dictionary<double, Asteroid>();
+            foreach (var asteroid in asteroids)
+            {
+                var angle = forAsteroid.GetAngle(asteroid);
+                if (!check.Any(x => Math.Abs(x.Key - angle) < double.Epsilon) 
+                    && !asteroid.Equals(forAsteroid))
+                {
+                    result++;
+                    check.Add(angle, asteroid);
+                }
+            }
+
+            return result;
+        }
+
+        private List<KeyValuePair<double, Asteroid>> FindAllViewableOrdered(Asteroid forAsteroid, List<Asteroid> asteroids)
+        {
+            var result = 0;
+            var check = new Dictionary<double, (Asteroid, double Distance)>();
+            foreach (var asteroid in asteroids.Where(x => !x.Equals(forAsteroid)))
+            {
+                var angle = forAsteroid.GetAngleDegree(asteroid);
+                if (!check.Any(x => Math.Abs(x.Key - angle) < double.Epsilon))
+                {
+                    result++;
+                    check.Add(angle, (asteroid, forAsteroid.GetLength(asteroid)));
+                }
+                else
+                {
+                    var distance = forAsteroid.GetLength(asteroid);
+                    var first = check.First(x => Math.Abs(x.Key - angle) < double.Epsilon);
+
+                    if (distance < first.Value.Item2)
+                        check[first.Key] = (asteroid, distance);
+                }
+            }
+
+            var res = check.OrderBy(x => x.Key).Select(x => new KeyValuePair<double, Asteroid>(x.Key, x.Value.Item1))
+                .ToList();
+
+            return res;
+        }
+
+        private static List<Asteroid> GetAsteroids(string inputFile)
+        {
+            var lines = File.ReadAllLines(inputFile);
+
+            //            var lines = @".#..#
+            //.....
+            //#####
+            //....#
+            //...##".Split(new[] { Environment.NewLine }, StringSplitOptions.None);            
+
+            //            var lines = @".#..##.###...#######
+            //##.############..##.
+            //.#.######.########.#
+            //.###.#######.####.#.
+            //#####.##.#.##.###.##
+            //..#####..#.#########
+            //####################
+            //#.####....###.#.#.##
+            //##.#################
+            //#####.##.###..####..
+            //..######..##.#######
+            //####.##.####...##..#
+            //.#####..#.######.###
+            //##...#.##########...
+            //#.##########.#######
+            //.####.#.###.###.#.##
+            //....##.##.###..#####
+            //.#.#.###########.###
+            //#.#.#.#####.####.###
+            //###.##.####.##.#..##".Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            var result = new List<Asteroid>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (line[j] == '#')
+                        result.Add(new Asteroid(j, i));
+                }
+            }
+
+            return result;
+        }
+    }
+
+
+
+    [DebuggerDisplay("X = {X} Y = {Y}")]
+    internal struct Asteroid : IEquatable<Asteroid>
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Asteroid(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double GetAngle(Asteroid target)
+        {
+            var angle = Math.Atan2(target.Y - Y, target.X - X);
+
+            if (angle < 0)
+            {
+                angle += Math.Ceiling(-angle / 360) * 360;
+            }
+
+            return angle;
+        }
+
+        public double GetAngleDegree(Asteroid target)
+        {
+            double xDiff = target.X - X;
+            double yDiff = target.Y - Y;
+            var res = Math.Atan2(yDiff, xDiff) * 180.0 / Math.PI + 90;
+            if (res < 0)
+
+                res += 360;
+
+            return res;
+        }
+
+        public double GetLength(Asteroid target)
+        {
+            double xDiff = target.X - X;
+            double yDiff = target.Y - Y;
+            return Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+
+        public bool Equals(Asteroid other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Asteroid other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X * 397) ^ Y;
+            }
+        }
+    }
+}
--- AdventOdCode2019/Program.cs
+++ AdventOdCode2019/Program.cs
@@ -7,29 +7,31 @@ namespace AdventOdCode2019
         private static void Main(string[] args)
         {
             RunMe();
-            RunAlex();
+            //RunAlex();
         }
 
         private static void RunMe()
         {
-            new Day1Part1().Calculate("input/day1part1.txt").Dump(nameof(Day1Part1));
-            new Day1Part2().Calculate("input/day1part1.txt").Dump(nameof(Day1Part2));
-            new Day2().CalculatePart1("input/day2.txt").Dump(nameof(Day2));
-            new Day2().CalculatePart2("input/day2.txt").Dump(nameof(Day2));
-            new Day3().CalculatePart1("input/day3.txt").Dump(nameof(Day3));
-            new Day3().CalculatePart2("input/day3.txt").Dump(nameof(Day3));
-            new Day4().CalculatePart1(string.Empty).Dump(nameof(Day4));
-            new Day4().CalculatePart2(string.Empty).Dump(nameof(Day4));
-            new Day5().CalculatePart1("input/day5.txt").Dump(nameof(Day5));
-            new Day5().CalculatePart2("input/day5.txt").Dump(nameof(Day5));
-            new Day6().CalculatePart1("input/day6.txt").Dump(nameof(Day6));
-            new Day6().CalculatePart2("input/day6.txt").Dump(nameof(Day6));
-            new Day7().CalculatePart1("input/day7.txt").Dump(nameof(Day7));
-            new Day7().CalculatePart2("input/day7.txt").Dump(nameof(Day7));
-            new Day8().CalculatePart1("input/day8.txt").Dump(nameof(Day8));
-            new Day8().CalculatePart2("input/day8.txt").Dump(nameof(Day8));
-            new Day9().CalculatePart1("input/day9.txt").Dump(nameof(Day9));
-            new Day9().CalculatePart2("input/day9.txt").Dump(nameof(Day9));
+            //new Day1Part1().Calculate("input/day1part1.txt").Dump(nameof(Day1Part1));
+            //new Day1Part2().Calculate("input/day1part1.txt").Dump(nameof(Day1Part2));
+            //new Day2().CalculatePart1("input/day2.txt").Dump(nameof(Day2));
+            //new Day2().CalculatePart2("input/day2.txt").Dump(nameof(Day2));
+            //new Day3().CalculatePart1("input/day3.txt").Dump(nameof(Day3));
+            //new Day3().CalculatePart2("input/day3.txt").Dump(nameof(Day3));
+            //new Day4().CalculatePart1(string.Empty).Dump(nameof(Day4));
+            //new Day4().CalculatePart2(string.Empty).Dump(nameof(Day4));
+            //new Day5().CalculatePart1("input/day5.txt").Dump(nameof(Day5));
+            //new Day5().CalculatePart2("input/day5.txt").Dump(nameof(Day5));
+            //new Day6().CalculatePart1("input/day6.txt").Dump(nameof(Day6));
+            //new Day6().CalculatePart2("input/day6.txt").Dump(nameof(Day6));
+            //new Day7().CalculatePart1("input/day7.txt").Dump(nameof(Day7));
+            //new Day7().CalculatePart2("input/day7.txt").Dump(nameof(Day7));
+            //new Day8().CalculatePart1("input/day8.txt").Dump(nameof(Day8));
+            //new Day8().CalculatePart2("input/day8.txt").Dump(nameof(Day8));
+            //new Day9().CalculatePart1("input/day9.txt").Dump(nameof(Day9));
+            //new Day9().CalculatePart2("input/day9.txt").Dump(nameof(Day9));
+            //new Day10().CalculatePart1("input/day10.txt").Dump(nameof(Day10));
+            new Day10().CalculatePart2("input/day10.txt").Dump(nameof(Day10));
         }
 
         private static void RunAlex()
