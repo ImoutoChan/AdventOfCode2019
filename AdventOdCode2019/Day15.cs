562bd99798819f7c0af3e49741f81032a0b92184
ImoutoChan <kirirrin@gmail.com>
2019-12-17 00:33:23 +0300

    Day15 Part 2

--- AdventOdCode2019/Day15.cs
+++ AdventOdCode2019/Day15.cs
@@ -6,15 +6,87 @@ using System.Linq;
 
 namespace AdventOdCode2019
 {
+    public class Droid
+    {
+        private readonly IntCodeRunner9 _runner;
+        private readonly DroidPoint _homePoint = new DroidPoint(0, 0, null);
+        private readonly Stack<Movement> _fromHome = new Stack<Movement>();
+
+        private DroidPoint _position = new DroidPoint(0, 0, null);
+
+        public Droid(long[] program)
+        {
+            _runner = new IntCodeRunner9(program);
+        }
+
+        public Status Move(Movement movement)
+        {
+            var status = (Status)_runner.Run((int)movement);
+            if (status != Status.Wall)
+            {
+                _fromHome.Push(movement);
+                _position = _position.GetPoint(movement);
+            }
+
+            return status;
+        }
+
+        public Status Move(DroidPoint point)
+        {
+            Status status = Status.Empty;
+
+            var currentPoint = point;
+            foreach (var pathPoint in point.GetPathToHome().Reverse().Skip(1).Append(point))
+            {
+                var movement = _position.GetMovement(pathPoint);
+                status = Move(movement);
+            }
+
+            return status;
+        }
+
+        public void ReturnHome()
+        {
+            while (_fromHome.TryPop(out var movement))
+            {
+                var moveTo = GetReverse(movement);
+                var status = (Status)_runner.Run((int)moveTo);
+                _position = _position.GetPoint(moveTo);
+
+                if (status != Status.Empty)
+                    throw new Exception("Invalid returning home");
+            }
+
+            if (!_position.Equals(_homePoint))
+                throw new Exception("Invalid returning home");
+        }
+
+        private Movement GetReverse(Movement movement)
+        {
+            switch (movement)
+            {
+                case Movement.North:
+                    return Movement.South;
+                case Movement.South:
+                    return Movement.North;
+                case Movement.West:
+                    return Movement.East;
+                case Movement.East:
+                    return Movement.West;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(movement), movement, null);
+            }
+        }
+    }
+
     internal class Day15 : IAdventOfCodeDay
     {
         public string CalculatePart1(string inputFile)
         {
             var program = GetProgram(inputFile);
+            var droid = new Droid(program);
 
-            var runner = new IntCodeRunner9(program);
-
-            var startPoint = new DroidPoint(0, 0, Enumerable.Empty<DroidPoint>());
+            var startPoint = new DroidPoint(0, 0, null);
             var map = new Dictionary<DroidPoint, Status>();
             map.Add(startPoint, Status.Empty);
 
@@ -30,7 +102,7 @@ namespace AdventOdCode2019
                 if (map.ContainsKey(currentPoint))
                     continue;
 
-                var status = CheckCurrentPoint(currentPoint);
+                var status = CheckCurrentPoint(currentPoint, droid);
                 map.Add(currentPoint, status);
 
                 if (status == Status.Empty)
@@ -43,7 +115,7 @@ namespace AdventOdCode2019
 
                 else if (status == Status.Oxygen)
                 {
-                    return currentPoint.HomePath.Count().ToString();
+                    return (currentPoint.HomePath.GetPathToHome().Count() + 1).ToString();
                 }
             }
 
@@ -55,60 +127,79 @@ namespace AdventOdCode2019
                 if (!map.ContainsKey(newPoint))
                     queue.Enqueue(newPoint);
             }
+        }
+        
+        private Status CheckCurrentPoint(DroidPoint currentPoint, Droid droid)
+        {
+            var statusResult = droid.Move(currentPoint);
+            droid.ReturnHome();
 
-            Status CheckCurrentPoint(DroidPoint currentPoint)
+            return statusResult;
+        }
+        
+        public string CalculatePart2(string inputFile)
+        {
+            var program = GetProgram(inputFile);
+            var droid = new Droid(program);
+            
+            var startPoint = new DroidPoint(0, 0, null);
+            var map = new Dictionary<DroidPoint, Status>();
+            map.Add(startPoint, Status.Empty);
+
+            var queue = new Queue<DroidPoint>();
+            EnqueuePoints(startPoint, Movement.East);
+            EnqueuePoints(startPoint, Movement.West);
+            EnqueuePoints(startPoint, Movement.North);
+            EnqueuePoints(startPoint, Movement.South);
+
+            while (queue.Any())
             {
-                if (currentPoint.Equals(startPoint))
-                    return Status.Empty;
+                var currentPoint = queue.Dequeue();
+                if (map.ContainsKey(currentPoint))
+                    continue;
 
-                var imPoint = startPoint;
-                foreach (var pathPoint in currentPoint.HomePath.Skip(1))
+                var status = CheckCurrentPoint(currentPoint, droid);
+                map.Add(currentPoint, status);
+
+                if (status != Status.Wall)
                 {
-                    var movement = imPoint.GetMovement(pathPoint);
-                    var res = runner.Run((int)movement);
-                    imPoint = imPoint.GetPoint(movement);
-                    if (res != 1)
-                        throw new Exception();
+                    EnqueuePoints(currentPoint, Movement.East);
+                    EnqueuePoints(currentPoint, Movement.West);
+                    EnqueuePoints(currentPoint, Movement.North);
+                    EnqueuePoints(currentPoint, Movement.South);
                 }
+            }
 
-                var currentPointMovement = imPoint.GetMovement(currentPoint);
-                var statusResult = (Status)runner.Run((int)currentPointMovement);
-                
-                if (statusResult == Status.Empty)
-                {
-                    imPoint = imPoint.GetPoint(currentPointMovement);
-                }
-                else if(statusResult == Status.Oxygen)
-                {
-                    return statusResult;
-                }
+            return FindFullBreadth(map).ToString();
 
-                if (!imPoint.Equals(startPoint))
-                {
-                    foreach (var pathPoint in currentPoint.HomePath.Reverse())
-                    {
-                        if (imPoint.Equals(pathPoint))
-                            continue;
-
-                        var movement = imPoint.GetMovement(pathPoint);
-                        var res = runner.Run((int) movement);
-                        imPoint = imPoint.GetPoint(movement);
-                        if (res != 1)
-                            throw new Exception();
-                    }
-                }
-
-                if (!imPoint.Equals(startPoint))
-                    throw new Exception();
-
-                return statusResult;
+            void EnqueuePoints(DroidPoint currentPoint, Movement movement)
+            {
+                var newPoint = currentPoint.GetPoint(movement);
+                if (!map.ContainsKey(newPoint))
+                    queue.Enqueue(newPoint);
             }
         }
 
-
-        public string CalculatePart2(string inputFile)
+        private int FindFullBreadth(Dictionary<DroidPoint, Status> map)
         {
-            throw new NotImplementedException();
+            var search = new BreadthFirstSearch<DroidPoint>(MoveFunc);
+            int max = 0;
+            search.GetShortestPathLength(
+                map.Single(x => x.Value == Status.Oxygen).Key,
+                (_, depth) =>
+                {
+                    max = Math.Max(max, depth);
+                    return false;
+                });
+
+            return max;
+
+            IEnumerable<DroidPoint> MoveFunc(DroidPoint point)
+            {
+                return point
+                    .GetNearPoints()
+                    .Where(x => map.TryGetValue(x, out var status) && status != Status.Wall);
+            }
         }
 
         private static long[] GetProgram(string inputFile)
@@ -120,19 +211,29 @@ namespace AdventOdCode2019
     }
 
     [DebuggerDisplay("X = {X} Y = {Y}")]
-    internal struct DroidPoint : IEquatable<DroidPoint>
+    public class DroidPoint : IEquatable<DroidPoint>
     {
         public int X { get; }
 
         public int Y { get; }
 
-        public IEnumerable<DroidPoint> HomePath { get; }
+        public DroidPoint HomePath { get; }
 
-        public DroidPoint(int x, int y, IEnumerable<DroidPoint> homePath)
+        public IEnumerable<DroidPoint> GetPathToHome()
+        {
+            var current = HomePath;
+            while (current != null)
+            {
+                yield return current;
+                current = current.HomePath;
+            }
+        }
+
+        public DroidPoint(int x, int y, DroidPoint homePath)
         {
             X = x;
             Y = y;
-            HomePath = homePath.ToList();
+            HomePath = homePath;
         }
 
         public DroidPoint GetPoint(Movement direction)
@@ -140,16 +241,23 @@ namespace AdventOdCode2019
             switch (direction)
             {
                 case Movement.North:
-                    return new DroidPoint(X, Y + 1, HomePath.Append(this));
+                    return new DroidPoint(X, Y + 1, this);
                 case Movement.East:
-                    return new DroidPoint(X + 1, Y, HomePath.Append(this));
+                    return new DroidPoint(X + 1, Y, this);
                 case Movement.South:
-                    return new DroidPoint(X, Y - 1, HomePath.Append(this));
+                    return new DroidPoint(X, Y - 1, this);
                 case Movement.West:
-                    return new DroidPoint(X - 1, Y, HomePath.Append(this));
+                    return new DroidPoint(X - 1, Y, this);
                 default:
                     throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
             }
+        }
+        public IEnumerable<DroidPoint> GetNearPoints()
+        {
+            yield return GetPoint(Movement.East);
+            yield return GetPoint(Movement.West);
+            yield return GetPoint(Movement.North);
+            yield return GetPoint(Movement.South);
         }
 
         public Movement GetMovement(DroidPoint pathPoint)
--- AdventOdCode2019/Day6.cs
+++ AdventOdCode2019/Day6.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -80,56 +79,6 @@ namespace AdventOdCode2019
             var result = graph.GetShortestPathLength(me.Item1, s => s == sun.Item1);
 
             return result;
-        }
-    }
-
-    public class BreadthFirstSearch<TElement>
-    {
-        private readonly Func<TElement, IEnumerable<TElement>> _moveFunc;
-
-        public BreadthFirstSearch(Func<TElement, IEnumerable<TElement>> moveFunc)
-        {
-            _moveFunc = moveFunc;
-        }
-
-        public int GetShortestPathLength(TElement initialElement, Func<TElement, bool> isFinishFunc)
-        {
-            var queue = new Queue<SearchElement<TElement>>();
-            var visited = new List<TElement>();
-
-            queue.Enqueue(new SearchElement<TElement>(initialElement, 0));
-
-            while (queue.Any())
-            {
-                var current = queue.Dequeue();
-
-                if (isFinishFunc(current.Element))
-                    return current.Depth;
-
-                visited.Add(current.Element);
-
-                var newElements = _moveFunc(current.Element)
-                                  .Where(x => !visited.Contains(x))
-                                  .Select(x => new SearchElement<TElement>(x, current.Depth + 1));
-
-                foreach (var searchElement in newElements)
-                    queue.Enqueue(searchElement);
-            }
-
-            return -1;
-        }
-
-        private class SearchElement<T>
-        {
-            public SearchElement(T element, int depth)
-            {
-                Element = element;
-                Depth = depth;
-            }
-
-            public T Element { get; }
-
-            public int Depth { get; }
         }
     }
 }
